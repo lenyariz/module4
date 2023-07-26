8932cf0b12bca55a861dae4473ce42ad93c91076
MILKIS <lenyarizov.rizov@mail.ru>
2023-07-26 20:46:11 +0300

    Домашка к уроку №4

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("advertisements.urls"))
+    path('', include("advertisements.urls")),
+    path('', include("app_lesson_4.urls")),
 ]
